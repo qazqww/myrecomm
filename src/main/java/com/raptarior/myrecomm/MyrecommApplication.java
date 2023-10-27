6080d5a5b24fa30f02417cc2969aad1c3db3b2e0
raptarior <qazqww@naver.com>
2023-10-27 14:23:50 +0900

    First init

---
+++ src/main/java/com/raptarior/myrecomm/MyrecommApplication.java
@@ -0,0 +1,13 @@
+package com.raptarior.myrecomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrecommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyrecommApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raptarior/myrecomm/MyrecommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raptarior.myrecomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrecommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
